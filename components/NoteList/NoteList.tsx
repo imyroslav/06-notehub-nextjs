4d485697d6cbc14773078eb3fecff83c96a603a1
imyroslav <imyroslav@ukr.net>
2025-06-30 07:33:10 +0200

    update-dynamic-route

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,6 +1,6 @@
 import { type NoteList } from "../../lib/api";
-import NoteItem from "../../app/notes/id/page";
-// import css from "./NoteList.module.css"
+import NoteItem from "../../app/notes/[id]/page";
+import css from "./NoteList.module.css"
 
 
 type NoteListProps = {
